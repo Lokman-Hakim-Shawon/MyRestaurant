8ad09ab9c68badfd53fc3189b675b890d601dd81
lokman hakim <lokmanhakimshawon3@gmail.com>
2025-08-05 19:40:35 +0600

    update code

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,48 +13,48 @@ import img4 from '../assets/home/slide4.jpg'
 import img5 from '../assets/home/slide5.jpg'
 const Category = () => {
     return (
-        <div className='my-4 '>
+        <div className='my-16 '>
       <Swiper
-        slidesPerView={4}
+        slidesPerView={3}
         spaceBetween={20}
         freeMode={true}
         pagination={{
           clickable: true,
         }}
         modules={[FreeMode, Pagination]}
-        className="mySwiper lg:my-4"
+        className="mySwiper lg:my-4 min-h-[1005]"
       >
         <SwiperSlide>
-             <img src={img1} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>salads</h1>
+             <img src={img1} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>salads</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img2} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>soups</h1>
+             <img src={img2} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>soups</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img3} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>pizzas</h1>
+             <img src={img3} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>pizzas</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img4} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>desserts</h1>
+             <img src={img4} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>desserts</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img5} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>salads</h1>
+             <img src={img5} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>salads</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img2} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>soups</h1>
+             <img src={img2} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>soups</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img3} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>pizzas</h1>
+             <img src={img3} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>pizzas</h1>
         </SwiperSlide>
         <SwiperSlide>
-             <img src={img4} />
-             <h1 className='lg:text-4xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>desserts</h1>
+             <img src={img4} className='h-full'/>
+             <h1 className='lg:text-2xl uppercase text-center lg:-mt-16 text-white hidden lg:block'>desserts</h1>
         </SwiperSlide>
       </Swiper>
     </div>
--- src/components/Testimonia.jsx
+++ src/components/Testimonia.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import img from '../assets/home/chef-service.jpg'
 const Testimonia = () => {
     return (
-        <div className='py-4 h-[80vh] flex items-center justify-center my-4 bg-cover bg-center' style={{backgroundImage: `url(${img})`}}>
+        <div className='py-4 h-[50vh] lg:h-[80vh] flex items-center justify-center my-4 bg-cover bg-center' style={{backgroundImage: `url(${img})`}}>
             {/* <img src={img} /> */}
-            <div className='z-10  text-center bg-white w-[80%] lg:w-[80%] lg:p-24 py-4'>
+            <div className='z-10  text-center bg-white w-[80%] lg:w-[80%] lg:p-24 py-4 text-black'>
                 <h1 className="lg:text-3xl lg:py-4">My Restaurant</h1>
                 <p className='text-xs lg:text-lg px-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
             </div>
